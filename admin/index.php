755fc0d0837f90f60804db43a9f0ca33da723056
Ander Elortondo <ander.elor@gmail.com>
2026-06-25 14:43:04 +0200

    Konpontzen adminera sarbidea

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,13 @@
 <?php
 $cfg = require __DIR__ . '/config.php';
+// FastCGI/PHP-FPM-rekin PHP_AUTH_USER hutsa egon daiteke; Authorization header-etik atera
+if (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] === '') {
+    $h = $_SERVER['HTTP_AUTHORIZATION'] ?? $_SERVER['REDIRECT_HTTP_AUTHORIZATION'] ?? '';
+    if (preg_match('/^Basic\s+(.+)$/i', $h, $m)) {
+        [$_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']] =
+            array_pad(explode(':', base64_decode($m[1]), 2), 2, '');
+    }
+}
 $u = $_SERVER['PHP_AUTH_USER'] ?? '';
 $p = $_SERVER['PHP_AUTH_PW'] ?? '';
 if (!hash_equals($cfg['auth_user'], $u) || !hash_equals($cfg['auth_pass'], $p)) {
